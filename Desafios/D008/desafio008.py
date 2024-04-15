e7447664893d027636c4972554a24f8ac33202a9
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2024-04-15 13:40:08 -0300

    Desafio 008 - Curso

---
+++ Desafios/D008/desafio008.py
@@ -0,0 +1,7 @@
+numero = int(input('Escreva um valor em metros: '))
+
+centimetros = numero/100
+milimetros = numero/1000
+
+print('{} metros é igual a {}cm'.format(numero,centimetros))
+print('{} metros é igual a {}mm'.format(numero,milimetros))
