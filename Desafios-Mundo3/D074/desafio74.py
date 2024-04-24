58236c922623e5c87f40c8113dcb987aa2967e4b
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2024-04-24 10:03:51 -0300

    Desafio 074 - Curso

---
+++ Desafios-Mundo3/D074/desafio74.py
@@ -0,0 +1,6 @@
+from random import randint
+tupla = (randint(1,100),randint(1,100),randint(1,100),randint(1,100),randint(1,100))
+
+print(f'A tupla gerada foi: {tupla}')
+print(f'O menor número dessa tupla é: {min(tupla)}')
+print(f'O maior número desssa tupla é {max(tupla)}')
