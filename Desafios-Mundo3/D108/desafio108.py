23a1b89b34ac5cfe4910784ef0b9fde7c8dc2ace
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2024-04-30 19:35:08 -0300

    Desafio 108 - Curso

---
+++ Desafios-Mundo3/D108/desafio108.py
@@ -0,0 +1,5 @@
+import moeda
+n = float(input('Digite o preço: R$'))
+print(f'A metade de {moeda.moeda(n)} é {moeda.moeda(moeda.metade(n))}')
+print(f'O dobro de {moeda.moeda(n)} é {moeda.moeda(moeda.dobro(n))}')
+print(f'Aumentando 10%, temos {moeda.moeda(moeda.aumentar(n,10))}')
